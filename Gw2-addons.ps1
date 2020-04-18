3f02b47e830e91ad7e7662dcfdbf6e4fddfc4ec4
Daniel Rothgänger <drothgaenger@googlemail.com>
2020-04-18 16:26:44 +0200

    initial commit with version 0.1

---
+++ Gw2-addons.ps1
@@ -0,0 +1,186 @@
+<#
+.Synopsis
+   Downloads addons for Guild Wars 2
+.DESCRIPTION
+   Fancy long description - TBD at 1.0
+.EXAMPLE
+    Nothing special yet, a lot todo, so no fancy arguments. Also it might get interactive...
+    .\gw2-addons.ps1
+
+.NOTES
+    Author: Daniel Rothgänger, mail@redicio.us
+    Version: 0.1
+    Date: 2020-04-18
+
+    Todos: 
+     - Setup Gitlab project
+     - self-update
+     - configuration file for preferences + Smart chainload with custom configs
+     - outsource $addons[]
+    
+     nice to have features:
+     - caching for downloads
+     - parallel downloading for all addons
+    
+     Known limitations: 
+     - assumes that addons are a single file download (one file or zipped)
+
+.FUNCTIONALITY
+   TBD at 1.0
+
+#>
+
+[cmdletbinding()]
+Param()
+
+Write-Verbose "Verbose output enabled"
+
+#statics
+$TempDir = ($env:TEMP)+"\GW2Addons\"
+
+
+# functions
+function Set-ShortCutOnDesktop
+{
+    param(
+        [string]$ShortCutName,
+        [string]$Exe
+    )
+    $ShortCutPath = "$Home\Desktop\"+$ShortCutName+".lnk"
+    $WshShell = New-Object -comObject WScript.Shell -erroraction stop
+    $Shortcut = $WshShell.CreateShortcut($ShortCutPath)
+    $Shortcut.TargetPath = $Exe
+    $Shortcut.WorkingDirectory = split-path ($Exe)
+    $Shortcut.Save()
+
+}
+
+
+
+# check in correct dir - why not search for gw2 install folder? because GW2 is quite flexible about the installation dir - you could have multiple copies and run it from everywhere and the registry contains only the one that has used the official installer for uninstallation
+if(!((test-path ".\bin64") -and (test-path "Gw2-64.exe")))
+{
+    "You are not running this from an actual Guild Wars 2 Directory!"
+    Throw "You are not running this from an actual Guild Wars 2 Directory!"
+    return # in case you have some weird ErrorAction defaults
+}
+
+# check game runnning
+if((get-process *gw* | ?{ $_.imagepath -eq (get-item "GW2-64.exe").fullname}).count -gt 0)
+{
+    write-warning "The game is running! Closing the game is recommended before patching - but you can go on at your own risk!"
+    write-warning "Press any key to continue, or CTRL+C to abort"
+    pause
+}
+
+$DateStart = get-date
+
+# actual downloading\unzipping\copying 
+try
+{
+
+    # take care of temp dir: clear or create
+    if(test-path $TempDir)
+    {
+        gci $TempDir | remove-item -recurse -force -erroraction stop | out-null
+    }
+    else 
+    {
+        new-item -type Directory -Path $TempDir -force -erroraction stop | out-null   
+    }
+
+    $Addons=@(
+        @{
+            Name="Radial Mount"; 
+            DL= ("https://github.com"+(((Invoke-WebRequest https://github.com/Friendly0Fire/GW2Radial/releases/latest -UseBasicParsing).content -split "`r`n" | select-string -pattern "`"\/.*GW2Radial\.zip`"" -AllMatches).matches.groups[0].value -replace '"'));
+            Type="Zip"
+            CopyJobs = @(@{from="d3d9.dll";to="bin64\d3d9_chainload.dll"})
+        },
+        @{
+            Name="TacO";
+            DL = (((Invoke-WebRequest http://www.gw2taco.com/ -UseBasicParsing).content | select-string -pattern "<a href='(.*)'.*\s\d{3}\.\d{4}r" -AllMatches).matches[0].groups[1].value -replace "dl=0$","dl=1");
+            Type="Zip";
+            CopyJobs = @(@{from="";to="";recurse=$true});
+            ShortCuts = @(@{exe=".\TacO\GW2TacO.exe"; name="GW2TacO"})
+        },
+        @{
+            Name="TacO-poi-tekkitworkshop";
+            DL = "http://tekkitsworkshop.net/index.php/gw2-taco/download/send/2-taco-marker-packs/32-all-in-one"
+            Type="directWName";
+            FileName = "tw_ALL_IN_ONE.taco"
+            CopyJobs = @(@{from="tw_ALL_IN_ONE.taco";to="TacO\POIs\tw_ALL_IN_ONE.taco"});
+            Comment = "Great POIs for Super adventure box, rich nodes and gardens!"
+        },
+        @{
+            Name="Arc DPS";
+            DL = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll";
+            Type="directWName"
+            FileName = "d3d9.dll"
+            CopyJobs = @(@{from="d3d9.dll";to="bin64\d3d9.dll"})
+
+        }
+    )
+
+    foreach($addon in ($addons |?{ $_.DL -ne $null}))
+    {
+        # get target for download
+        $TargetDLDir = $TempDir + $addon.name
+        switch($addon.type)
+        {
+            "direct"        { $TargetDL = $TargetDLDir + "\" }
+            "directWName"   { $TargetDL = $TargetDLDir + "\" + $addon.FileName }
+            "Zip"           { $TargetDL = $TargetDLDir + "." + $addon.type }
+            default         { Throw "Internal Error, unknown type $($addon.type)" }
+        }
+
+        if(!(test-path ($TempDir + $addon.name)))
+        {
+            new-item -type directory -path $TargetDLDir -force -erroraction stop | out-null
+        }
+
+        # Downloading remote files
+        "$($addon.dl) -> $($TargetDL)"
+        write-verbose "Invoke-WebRequest -uri $($addon.dl) -outfile $($TargetDL) -usebasicparsing -erroraction stop"
+        Invoke-WebRequest -uri $addon.dl -outfile $TargetDL -usebasicparsing -erroraction stop
+        start-sleep -milliseconds 100 # I/O is sometimes weird, too lazy to check for handles
+        if(!(test-path $TargetDL))
+        {
+            Throw "The download for addon $($addon.name) failed - maybe the payload was null"
+        }
+
+        if($TargetDL -match '\.zip$')
+        {
+            Expand-Archive -Path $TargetDL -DestinationPath ($TempDir+$addon.name+"\") -erroraction stop
+            start-sleep -milliseconds 100 # I/O is still sometimes weird, still too lazy to check for handles
+        }
+        
+        # Apply downloaded files for actual target
+        foreach($CopyJob in $addon.CopyJobs)
+        {
+            "$($TempDir+$addon.name)\$($CopyJob.from) -> $(".\"+$CopyJob.to)"
+            write-verbose "copy-item -path $($TempDir+$addon.name+'\'+$CopyJob.from) -destination $('.\'+$CopyJob.to) -recurse:$($CopyJob.recurse) -erroraction stop -force"
+            copy-item -path ($TempDir+$addon.name+'\'+$CopyJob.from) -destination ('.\'+$CopyJob.to) -recurse:$CopyJob.recurse -erroraction stop -force
+        }
+
+        # create application shortcuts on desktop
+        foreach($ShortCut in $addon.ShortCuts)
+        {
+            $Exe = (get-item -path ($ShortCut.exe) -erroraction stop).FullName
+            Set-ShortCutOnDesktop -exe $exe -ShortCutName $ShortCut.name
+        }
+        
+        # cleanup downloaded file
+        get-item $TargetDL | remove-item -erroraction silentlycontinue
+
+    }
+}
+catch
+{
+    Write-warning "oops, something did go wrong. Maybe this will help:"
+    Throw $_
+}
+finally
+{
+    remove-item $TempDir -Recurse -Force
+    "Execution took $((new-timespan -start $DateStart).Totalseconds) seconds"
+}
